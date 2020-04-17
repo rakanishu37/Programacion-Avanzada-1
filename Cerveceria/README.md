bffbeceb5c1d919ddb76e492cb209bddcc3b8885
Federico Anastasi <fede_3775@hotmail.com>
2020-04-17 14:51:21 -0300

    Readme actualizado y uso de String.format

--- Cerveceria/README.md
+++ Cerveceria/README.md
@@ -1,1 +1,39 @@
-#Test
+# Cerveceria
+Es un sistema en el cual se resuelva la problematica de Productor-Consumidor utilizando Threads de Java
+
+## Consumidores
+De la forma en que esta diseñado el codigo con un solo hilo alcanza para lograr que el stock llegue a 0     
+
+## Bloques Synchronized
+Los bloques synchronized se utilizan para evitar que dos o más hilos accedan al mismo tiempo a una seccion sencible del 
+codigo o tambien llamada seccion critica como por ejemplo operaciones que modifiquen el estado del recurso compartido 
+entre ellos.
+
+## Recurso Compartido
+El recurso compartido no es más que una estructura o conjunto de datos el cual se comparte entre hilos que pueden acceder
+o modificar y se debe asegurar su integridad para que no se produzcan errores 
+    
+## Formas de instanciar un Thread de Java  
+
+- Si una clase implementa la interface Runnable luego se debera pasar un objeto de ella en el constructor del Thread como
+argumento.
+```bash
+ClaseQueImplementaRunnable obj = new ClaseQueImplementaRunnable ();
+
+Thread t = new Thread(obj);
+
+t.start();
+```
+
+- Si una clase extiende de la clase thread con llamar su constructor alcanza 
+
+```bash
+ClaseQueExtiendeThread obj = new ClaseQueExtiendeThread();
+obj.start()
+```
+ 
+- Por ultimo llamando al constructor sin parametros de Thread se puede instanciar uno
+```bash            
+Thread t = new Thread();
+t.start();       
+```        
--- Cerveceria/src/Cerveceria/BeerHouse.java
+++ Cerveceria/src/Cerveceria/BeerHouse.java
@@ -3,71 +3,71 @@ package Cerveceria;
 import java.util.Random;
 
 public class BeerHouse {
-	private final int MAX_STOCK = 100;
-	private boolean available = false;
-	private boolean outOfMaterials = false;
-	private int stock = 0;
-	Random producingCapacityBeers;
+    private final int MAX_STOCK = 100;
+    private boolean available = false;
+    private boolean outOfMaterials = false;
+    private int stock = 0;
+    Random producingCapacityBeers;
 
-	public BeerHouse(Random producingCapacityBeers) {
-		this.producingCapacityBeers = producingCapacityBeers;
-	}
+    public BeerHouse(Random producingCapacityBeers) {
+        this.producingCapacityBeers = producingCapacityBeers;
+    }
 
-	public synchronized void produceBeer() throws Throwable {
-		int quantity = producingCapacityBeers.nextInt(25) + 1;
-		int newStock = 0;
+    public synchronized void produceBeer() throws Throwable {
+        int quantity = producingCapacityBeers.nextInt(25) + 1;
+        int newStock = 0;
 
-		while (available) {
-			try {
-				this.wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		available = true;
-
-		newStock = (this.stock + quantity);
-		if (newStock <= this.MAX_STOCK) {
-		    this.stock = newStock;
-			System.out.println(quantity + " beers have been produced");
-			System.out.println("Stock has: " + this.stock + " beers");
-		} else {
-			this.stock = this.MAX_STOCK;
-			System.out.println(newStock - this.MAX_STOCK + " beers have been produced");
-			System.out.println("Stock is now full, it has " + this.stock + " beers");
-			outOfMaterials = true;
-		}
-		this.notify();
-		if(outOfMaterials){
-			throw new Throwable("No hay mas materiales para producir");
-		}
-	}
-
-	public synchronized void consumeBeer(int quantityDrank) throws Throwable {
-		while (!available) {
-			try {
-				this.wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-
-		if(quantityDrank > stock){
-		    quantityDrank = stock;
-		    this.stock = 0;
+        while (available) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-		else{
+        available = true;
+
+        newStock = (this.stock + quantity);
+        if (newStock <= this.MAX_STOCK) {
+            this.stock = newStock;
+            System.out.println(quantity + " beers have been produced");
+            System.out.println(String.format("Stock has: %s beers", this.stock ));
+        } else {
+            this.stock = this.MAX_STOCK;
+            System.out.println(newStock - this.MAX_STOCK + " beers have been produced");
+			System.out.println(String.format("Stock is now full, it has %s beers", this.stock ));
+            outOfMaterials = true;
+        }
+        this.notify();
+        if(outOfMaterials){
+            throw new Throwable("No hay mas materiales para producir");
+        }
+    }
+
+    public synchronized void consumeBeer(int quantityDrank) throws Throwable {
+        while (!available) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(quantityDrank > stock){
+            quantityDrank = stock;
+            this.stock = 0;
+        }
+        else{
             this.stock = this.stock - quantityDrank;
         }
 
-		System.out.println(quantityDrank + " beers has drank.");
-		if(this.stock == 0 && outOfMaterials){
-			throw new Throwable("No hay mas cervezas");
-		}
-		System.out.println("Stock has: " + this.stock + " beers.");
-		if(!outOfMaterials){
-			available = false;
-			this.notify();
-		}
-	}
+        System.out.println(quantityDrank + " beers has drank.");
+        if(this.stock == 0 && outOfMaterials){
+            throw new Throwable("No hay mas cervezas");
+        }
+        System.out.println(String.format("Stock has: %s beers", this.stock ));
+        if(!outOfMaterials){
+            available = false;
+            this.notify();
+        }
+    }
 }
